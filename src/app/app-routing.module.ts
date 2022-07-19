28a8334a2688d28ced4df600b7321c07e5853a9c
bigmangcom <icanseeyou21235>
2022-07-19 12:56:41 +0500

    1.admin and user panel added 2. login page added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./user-panel/home/home.component";
+import {AdminComponent} from "./admin-panel/admin/admin.component";
+import {LoginComponent} from "./shared-component/login/login.component";
+import {NotFoundComponent} from "./shared-component/not-found/not-found.component";
+
+const routes: Routes = [
+  // {path: '', redirectTo: '/task', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NotFoundComponent},
+
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './user-panel/home/home.component';
+import { AdminDashboardComponent } from './admin-panel/admin-dashboard/admin-dashboard.component';
+import { AdminComponent } from './admin-panle/admin/admin.component';
+import { LoginComponent } from './shared-component/login/login.component';
+import { NotFoundComponent } from './shared-component/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminDashboardComponent,
+    AdminComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
